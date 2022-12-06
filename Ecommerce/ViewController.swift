1af85ad502a6330826f9ac76753917e9ab3c74d9
Vadim Sosnovsky <vadim73rus1234567@mail.ru>
2022-12-06 11:57:26 +0400

    Initial Commit

---
+++ Ecommerce/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ecommerce
+//
+//  Created by Вадим Сосновский on 06.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
